45303901bf3cfd422d76d42e6d66c5e45c1ff4e8
daneelsheleg <42346177+daneelsheleg@users.noreply.github.com>
2018-09-26 21:37:55 +0300

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const LiveReloadPlugin = require('webpack-livereload-plugin');
+const webpack = require('webpack');
+const path = require('path');
+
+
+
+
+module.exports = {
+    entry: "./app.js",
+    output: {
+        filename: "bundle.js",
+    },
+    devServer: {
+        index: "html.html",
+    },
+    watch: true,
+    module: {
+      rules: [{
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000'
+      },
+        {test: /\.scss$/,
+        use: [{
+          loader: "style-loader"
+        }, {
+          loader: "css-loader"
+        }, {
+          loader: "sass-loader",
+          options: {
+            includePaths: []
+          }
+        }]
+      }]
+    },
+    plugins: [
+      new LiveReloadPlugin({}),
+      new webpack.WatchIgnorePlugin([
+        path.join(__dirname, "node_modules")
+      ]),
+    ]
+}
